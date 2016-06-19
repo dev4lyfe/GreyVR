3e94c29dbd0b325ac51448ccb2252a26e42435fb
paleonluna <paleonluna@gmail.com>
2016-06-19 13:50:15 -0700

    added mouse and keys

---
+++ Assets/KeyboardCam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardCam : MonoBehaviour {
+
+
+	float moveSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveHorizontal(Input.GetAxis ("Horizontal"));
+		MoveVertical(Input.GetAxis ("Vertical"));
+
+	}
+
+	void MoveHorizontal(float direction) {
+		transform.Translate (Vector3.right * direction * moveSpeed, Space.World);
+	}
+
+	void MoveVertical(float direction) {
+		transform.Translate (Vector3.forward * direction * moveSpeed, Space.World);
+	}
+}
---
+++ Assets/MouseHandController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHandController : MonoBehaviour {
+
+	float moveSpeed = .4f;
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MoveHorizontal(Input.GetAxis("Mouse X"));
+		MoveVertical(Input.GetAxis("Mouse Y"));
+
+	}
+	void MoveHorizontal(float direction) {
+		
+		transform.Translate (Vector3.right * direction * moveSpeed, Space.World);
+	}
+
+	void MoveVertical(float direction) {
+		transform.Translate (Vector3.forward * direction * moveSpeed, Space.World);
+	}
+
+	bool CheckHorizontalBounds(float direction) {
+		return true;
+
+	}
+
+	bool CheckVerticalBounds(float direction) {
+		return true;
+
+
+	}
+}
